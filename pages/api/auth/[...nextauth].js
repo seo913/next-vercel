cc59d02588f97d53e4487363a9bc67a819820ba3
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-05-19 11:50:11 +0900

    oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,7 +13,7 @@ export const authOptions = {
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID_VER,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET_VER
-      }),
+      }), 
       
   ],
   
