73162ef4252f53cb92668d8c056ea7a3cb6e842c
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-05-21 19:16:50 +0900

    next-secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,16 +8,13 @@ export const authOptions = {
         //Github에서 발급받은ID
       clientId: process.env.GITHUB_ID_VER,
       // clientId: process.env.GITHUB_ID,
-
-    //   Github에서 발급받은Secret
+      //   Github에서 발급받은Secret
       clientSecret: process.env.GITHUB_SECRET_VER
       // clientSecret: process.env.GITHUB_SECRET
-
     }),
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID_VER,
         // clientId: process.env.GOOGLE_CLIENT_ID,
-
         clientSecret: process.env.GOOGLE_CLIENT_SECRET_VER
         // clientSecret: process.env.GOOGLE_CLIENT_SECRET
       }), 
@@ -25,6 +22,6 @@ export const authOptions = {
   ],
   
   //jwt생성시쓰는암호 (아무거나 암호 쓰기)
-  secret : process.env.NEXTAUTH_URL,
+  secret : process.env.NEXTAUTH_SECRET,
 };
 export default NextAuth(authOptions); 
